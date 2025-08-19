0ba4160d01862de3e1ad381adcc0965981260cee
yusufABC <yusufabdullah20000@gmail.com>
2025-08-19 19:55:17 +0600

    added 07 no problem

---
+++ 06 SumOfArray.js
@@ -0,0 +1,8 @@
+
+
+const sumOfArray=(arr)=>{
+   const sum= arr.reduce((a,b)=> a+b)
+   console.log(sum)
+}
+
+sumOfArray([2,3,4,6,12])
---
+++ 07 Even.js
@@ -0,0 +1,7 @@
+// Problem 7: Find Even Numbers in an Array
+// Write a function that returns all even numbers from a given array.
+
+// Example:
+
+// Input: [1, 2, 3, 4, 5, 6]
+// Output: [2, 4, 6]
